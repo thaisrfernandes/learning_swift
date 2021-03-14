e5d80ab6bf339f1c0c4ed36cbfb6496aaf370103
Thaís Fernandes <throdriguesfernandes@gmail.com>
2021-03-14 13:49:47 -0300

    removing playground

--- Types and Variables/TypesAndVariables.swift
+++ Types and Variables/TypesAndVariables.swift
@@ -1,15 +1,17 @@
-import UIKit
+import Foundation
 
-/*:
- **Variables**
-*/
+//Variables
 
 var str: String = "Hello, playground"
 
-/*:
- `var str: Int` -> *error cause youd be assigning a text to an Int variable*
+/*
+ 
+ var str: Int -> error cause youd be assigning a text to an Int variable
+ 
  and
- `str = 1` --> *error cause **str is a String*, you cant assign a number to a String type variable**
+ 
+ str = 1 --> error cause **str is a String, you cant assign a number to a String type variable
+ 
 */
 
 print(str)
@@ -17,21 +19,23 @@ print(str)
 var a = 1
 var b = 1
 
-/*:
+/*
+ 
  the type doesnt need to be explict.
  
  once you assign, for example, an Int value to a variable, its type will be Int and it cant be changed.
  
  a variable value can be reassigned, but its type can't.
  
- `a = "one" ` --> error! Cause ***a** is an Int*, cause the *first value assigned to it was an Int.*
+ a = "one" --> error! Cause a is an Int, cause the first value assigned to it was an Int.
+ 
 */
 
 a = 2
 
 var myNumberOfApples = 19
 
-/*:
+/*
  variable names always in camelcase!
 */
 
@@ -42,21 +46,20 @@ print(a / b)
 
 let c = 10
 
-/*:
+/*
  
  let is a CONSTANT
  
- `c = 2` --> error! because **c* is a constant*.
+ c = 2 --> error! because c is a constant.
  
  constants cant have its value changed!
  
- using *let* makes it easier to debug and/or avoid bugs.
+ using let makes it easier to debug and/or avoid bugs.
  
  */
 
-/*:
- ---
- */
+
+//Data Types
 
 /*:
  Common Data Types:
@@ -71,15 +74,20 @@ var d: Float = 2.3;
 var e: Double = 13.9;
 var f: Bool = true;
 
-/*:
- **Converting Types**
+/*
+ 
+ Converting Types
+ 
  */
 
 print(Int(d))
 print(Int(e))
 
-/*:
- **Rounding numbers**
+/*
+ 
+ Rounding numbers
+ 
  */
 
 print(round(e))
+
